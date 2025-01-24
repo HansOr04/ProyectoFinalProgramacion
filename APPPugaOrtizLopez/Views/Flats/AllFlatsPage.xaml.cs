60f82a1301ab45a54b4e4d729f14551ae7015368
Mundrack <mateopuga75@gmail.com>
2025-01-24 17:55:31 -0500

    allflast

--- APPPugaOrtizLopez/Views/Flats/AllFlatsPage.xaml.cs
+++ APPPugaOrtizLopez/Views/Flats/AllFlatsPage.xaml.cs
@@ -1,9 +1,21 @@
+using APPPugaOrtizLopez.ViewModels;
+
 namespace APPPugaOrtizLopez.Views.Flats;
 
 public partial class AllFlatsPage : ContentPage
 {
-	public AllFlatsPage()
-	{
-		InitializeComponent();
-	}
+    public AllFlatsPage(AllFlatsViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        if (BindingContext is AllFlatsViewModel viewModel)
+        {
+            viewModel.LoadDepartamentosCommand.Execute(null);
+        }
+    }
 }
