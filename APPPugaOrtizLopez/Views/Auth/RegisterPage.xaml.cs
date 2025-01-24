f0c1a35f577d3a731c5fc81ad571feb216796f78
mattedlopez88 <matias.lopez@udla.edu.ec>
2025-01-24 17:53:09 -0500

    cs

--- APPPugaOrtizLopez/Views/Auth/RegisterPage.xaml.cs
+++ APPPugaOrtizLopez/Views/Auth/RegisterPage.xaml.cs
@@ -1,9 +1,12 @@
+using APPPugaOrtizLopez.ViewModels;
+
 namespace APPPugaOrtizLopez.Views.Auth;
 
 public partial class RegisterPage : ContentPage
 {
-	public RegisterPage()
-	{
-		InitializeComponent();
-	}
+    public RegisterPage(RegisterViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
