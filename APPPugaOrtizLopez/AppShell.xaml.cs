72dd8cdeeb926ccc1836494d9a71f4a1a8e70e74
HansOr04 <hansalazar04@gmail.com>
2025-01-25 19:40:31 -0500

    diseño

--- APPPugaOrtizLopez/AppShell.xaml.cs
+++ APPPugaOrtizLopez/AppShell.xaml.cs
@@ -10,6 +10,7 @@
 
         private void RegisterRoutes()
         {
+            Routing.RegisterRoute("Register", typeof(Views.Auth.RegisterPage));
             Routing.RegisterRoute("CreateFlat", typeof(Views.Flats.CreateFlatPage));
             Routing.RegisterRoute("FlatDetails", typeof(Views.Flats.FlatDetailsPage));
         }
